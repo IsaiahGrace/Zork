cbe5d287620f89a1658cf04dcfee4b362b5979b7
Zach <Zach@10.1.10.186>
2018-10-31 01:05:11 -0400

    Map now initializes all items first for Owner objects

--- Map.cpp
+++ Map.cpp
@@ -23,14 +23,25 @@ Map::Map(FILE *fptr) {
   //For each of the children of the Map node:
   xml_node<> *node = doc.first_node()->first_node();
 
-  //Two loops now. First loop initializes non-rooms, second initializes room objects
+  //THREE loops now. First loop initializes items, second initializes non room objects, third does rooms
   while(node) {
     string name = node->name();
     std::cout <<"Map: " << name << std::endl;
 
     //if(name == "room") rooms.push_back(Room(node));
     if(name == "item") items.push_back(Item(node));
-    else if(name == "container") containers.push_back(Container(node));
+    //else if(name == "container") containers.push_back(Container(node));
+    //else if(name == "creature") creatures.push_back(Creature(node));
+
+    node = node->next_sibling();
+  }
+
+  node = doc.first_node()->first_node();
+  while(node) {
+    string name = node->name();
+    std::cout <<"Map: " << name << std::endl;
+
+    if(name == "container") containers.push_back(Container(node));
     else if(name == "creature") creatures.push_back(Creature(node));
 
     node = node->next_sibling();
