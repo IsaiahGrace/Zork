4a1be2d86c6a1b66146c772dceb734a312921b51
Isaiah Grace <igrace@purdue.edu>
2018-11-01 18:52:21 -0400

    reformatted code

--- main.cpp
+++ main.cpp
@@ -12,69 +12,65 @@ using namespace std;
 // take(item) -- changes item ownership from room or container to inventory.
 
 int main(int argc, char **argv) {
-  // main method broken down into tasks
-  // 1. Initialization of the game
-  // 2. Finite State Machine (Running the game)
-  // 3. Cleanup after end of game
+    // main method broken down into tasks
+    // 1. Initialization of the game
+    // 2. Finite State Machine (Running the game)
+    // 3. Cleanup after end of game
 
-  // ******************
-  // Game Initialization
-  // ******************
-  if(argc != 2) {
-    cout << "Error, please specify one .xml map file (argc != 2)" << endl;
-    return EXIT_FAILURE;
-  }
-  FILE *fptr = fopen(argv[1], "r");
-  if(fptr == NULL) {
-    cout << "Error, fopen failed to find .xml file (fptr == NULL)" << endl;
-    return EXIT_FAILURE;
-  }
-
-
-  // Construct the Map object
-  Map map(fptr);
-  fclose(fptr);
-
-  // Create Player variables
-
-  // Context
-
-  // ******************************
-  // Start the Finite state Machine
-  // ******************************
-
-  try {
-    while(1){
-      //get current context
-      map.gameContext.parseContext();
-
-	  //check context for active noncommand triggers
-	  Trigger* commandTrigger = map.gameContext.checkNonCommandTriggers(&map);
-	  if(commandTrigger != NULL){
-		  commandTrigger->ExecuteTrigger(&map);
-	  }
-	  else{
-		  //parse the command line
-		  string command;
-		  cout<< ">";
-		  getline(cin,command);
-
-		  Trigger* nonCommandTrigger = map.gameContext.checkCommandTriggers(command,&map);
-		  if(nonCommandTrigger != NULL){
-			  nonCommandTrigger->ExecuteTrigger(&map);
-		  }
-		  else{
-			  map.parseAction(command);
-			  //parseAction should call the action func
-		  }
-	  }
+    // ******************
+    // Game Initialization
+    // ******************
+    if (argc != 2) {
+        cout << "Error, please specify one .xml map file (argc != 2)" << endl;
+        return EXIT_FAILURE;
+    }
+    FILE *fptr = fopen(argv[1], "r");
+    if (fptr == NULL) {
+        cout << "Error, fopen failed to find .xml file (fptr == NULL)" << endl;
+        return EXIT_FAILURE;
+    }
 
 
-  }
+    // Construct the Map object
+    Map map(fptr);
+    fclose(fptr);
 
-  }
-  catch (int i) {
-  std::cout << "Game Over" << std::endl;
-  return EXIT_SUCCESS;
-  }
+    // Create Player variables
+
+    // Context
+
+    // ******************************
+    // Start the Finite state Machine
+    // ******************************
+
+    try {
+        while (1) {
+            //get current context
+            map.gameContext.parseContext();
+
+            //check context for active noncommand triggers
+            Trigger *commandTrigger = map.gameContext.checkNonCommandTriggers(&map);
+            if (commandTrigger != NULL) {
+                commandTrigger->ExecuteTrigger(&map);
+            } else {
+                //parse the command line
+                string command;
+                cout << ">";
+                getline(cin, command);
+
+                Trigger *nonCommandTrigger = map.gameContext.checkCommandTriggers(command, &map);
+                if (nonCommandTrigger != NULL) {
+                    nonCommandTrigger->ExecuteTrigger(&map);
+                } else {
+                    map.parseAction(command);
+                    //parseAction should call the action func
+                }
+            }
+        }
+
+    }
+    catch (int i) {
+        std::cout << "Game Over" << std::endl;
+        return EXIT_SUCCESS;
+    }
 }
