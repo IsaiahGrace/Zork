e8d01556f25c45fc22dd818a4fe65c24b63198a9
Isaiah Grace <igrace@purdue.edu>
2018-10-31 23:36:08 -0400

    added gameOver() method to Map.hpp

--- Map.hpp
+++ Map.hpp
@@ -19,15 +19,17 @@ public:
   virtual ~Map() {};
   void parseAction(string);
   // Action methods (need implimentation) REMOVE {}
-  void move(string direction) {}; // Takes n,s,e,w and moves the player
-  void openExit() {}; // checks context and ends game if approppreate (call gameOver()? Throw exception?)
-  void take(string item) {}; // string will be the name of the item. Check context and add to inventory if approprate
-  void open(string container) {}; // string will be the name of the container. Check context and print out the names of items in the container if appropreate
-  void read(string item) {}; // string will be the name of an item. check context and print out the item description if appropreate
-  void drop(string item) {}; // string will be the name of an item. Check context and move item from inventory to room if appropreate
-  void put(string item, string container) {}; // move the item from the players inventory to the container if appropreate
-  void turnOn(string item) {}; // string will be the name of an item in player's inventory. activate the item if appropreate
-  void attack(string creature, string item) {}; // attack the creature with the item if item is in inventory and creature is in current room
+  void move(string direction);
+  void openExit();
+  void take(string item);
+  void open(string container);
+  void read(string item);
+  void drop(string item);
+  void put(string item, string container);
+  void turnOn(string item);
+  void attack(string creature, string item);
+  // Game over method throws exception
+  void gameOver();
   Room* getRoom(string);
   Owner* getOwner(string);
   Item* getItem(string);
