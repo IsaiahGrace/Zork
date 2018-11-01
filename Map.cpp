3634ba131d11189ab52ab024b898f483fca71859
Isaiah Grace <igrace@purdue.edu>
2018-11-01 16:28:48 -0400

    Now printing new room description

--- Map.cpp
+++ Map.cpp
@@ -286,6 +286,7 @@ void Map::move(string direction) {
         if (direction[0] == this->gameContext.currentRoom->borders[i].direction[0]) {
             this->gameContext.currentRoom = this->getRoom(this->gameContext.currentRoom->borders[i].name);
             std::cout << "moved " << direction << " to :" << this->gameContext.currentRoom->name << std::endl;
+            std::cout << this->gameContext.currentRoom->description << std::endl;
             return;
         }
     }
